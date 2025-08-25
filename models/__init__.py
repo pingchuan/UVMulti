436c12abfb39df2b627b47e809d54a56fc11d48f
pingchuan <130446779+pingchuan@users.noreply.github.com>
2025-08-25 16:45:01 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,1 @@
 
-from .models.masnet import  Net
-from .ResNet34UNet import ResNet34U1,PoseDecoder,ResNet34U_E,ResNet34UN,ResNet34U2
-from .models.deeplabv3 import deeplabv3plus_resnet50
-from .models.pspnet import PSPNet
-from .models.suimnet import SUIM_Net
-from .models.unext import UNext
-from .models.h2former import res34_swin_MS
-#from .models.segformer import Segformer
-from .models.segformer_b2 import SegFormer
-from .models.lanet_enh import LANet
-from .models.puienet import mynet
-from .models.WATERSNET import WaterSNet
-from .models.litnet import LitNet
